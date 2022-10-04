836f8e273173841ea03f5305d5e193d563fd486a
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-10-04 19:51:30 +0100

    Working on updating posts route

--- blog/main.py
+++ blog/main.py
@@ -36,7 +36,7 @@ def create_post(
     return new_post
 
 
-@app.get("/posts/{post_id}", status_code=status.HTTP_200_OK)
+@app.get("/post/{post_id}", status_code=status.HTTP_200_OK)
 def read_a_posts(post_id: int, db: Session = Depends(get_db)):
     posts = db.query(models.Post).filter(models.Post.id == post_id).first()
     if not posts:
@@ -46,9 +46,27 @@ def read_a_posts(post_id: int, db: Session = Depends(get_db)):
         )
     return posts
 
+
 @app.delete("/post/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(post_id: int, db: Session = Depends(get_db)):
-    db.query(models.Post).filter(models.Post.id == post_id).delete(synchronize_session=False)
+    post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    if post is not None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Content not found in DB")
+    post.delete(
+        synchronize_session=False
+    )
     db.commit()
     return "Post deleted"
 
+
+@app.put("/post/{post_id}", status_code=status.HTTP_202_ACCEPTED)
+def update_post(post_id: int, request: schemas.Post, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    if post is not None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with {post_id} not found",
+        )
+    post.update(request)
+    db.commit()
+    return "Post Updated Successfully"
