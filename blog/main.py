c663a445d72320d5637ca6a77d64edc0fdfe9665
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-10-04 20:23:42 +0100

    Put route now working perfectly

--- blog/main.py
+++ blog/main.py
@@ -17,12 +17,6 @@ def get_db():
         db.close()
 
 
-@app.get("/posts", response_model=list[schemas.Post], status_code=status.HTTP_200_OK)
-def read_posts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    posts = db.query(models.Post).offset(skip).limit(limit).all()
-    return posts
-
-
 @app.post("/post")
 def create_post(
     request: schemas.Post,
@@ -47,10 +41,13 @@ def read_a_posts(post_id: int, db: Session = Depends(get_db)):
     return posts
 
 
+
+
+
 @app.delete("/post/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(post_id: int, db: Session = Depends(get_db)):
-    post = db.query(models.Post).filter(models.Post.id == post_id).first()
-    if post is not None:
+    post = db.query(models.Post).filter(models.Post.id == post_id)
+    if not post.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Content not found in DB")
     post.delete(
         synchronize_session=False
@@ -58,15 +55,20 @@ def delete_post(post_id: int, db: Session = Depends(get_db)):
     db.commit()
     return "Post deleted"
 
-
 @app.put("/post/{post_id}", status_code=status.HTTP_202_ACCEPTED)
 def update_post(post_id: int, request: schemas.Post, db: Session = Depends(get_db)):
-    post = db.query(models.Post).filter(models.Post.id == post_id).first()
-    if post is not None:
+    post = db.query(models.Post).filter(models.Post.id == post_id)
+    if not post.first():
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Post with {post_id} not found",
         )
-    post.update(request)
+    post.update(dict(request))
     db.commit()
     return "Post Updated Successfully"
+
+
+@app.get("/posts", response_model=list[schemas.Post], status_code=status.HTTP_200_OK)
+def read_posts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    posts = db.query(models.Post).offset(skip).limit(limit).all()
+    return posts
--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,6 @@ from .database import Base
 
 class Post(Base):
     __tablename__ = "posts"
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     title = Column(String)
     body = Column(String)
