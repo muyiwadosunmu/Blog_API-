00110bd8b0a5aaee6d62435e016db5d25f0e8e2e
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-10-04 16:24:53 +0100

    Added Deleted Route

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, status, HTTPException, Depends
 from . import schemas, models
 from sqlalchemy.orm import Session
 from .database import SessionLocal, engine
@@ -17,14 +17,18 @@ def get_db():
         db.close()
 
 
-@app.get("/posts", response_model=list[schemas.Post])
+@app.get("/posts", response_model=list[schemas.Post], status_code=status.HTTP_200_OK)
 def read_posts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     posts = db.query(models.Post).offset(skip).limit(limit).all()
     return posts
 
 
 @app.post("/post")
-def create_post(request: schemas.Post, db: Session = Depends(get_db)):
+def create_post(
+    request: schemas.Post,
+    db: Session = Depends(get_db),
+    status_code=status.HTTP_201_CREATED,
+):
     new_post = models.Post(title=request.title, body=request.body)
     db.add(new_post)
     db.commit()
@@ -32,7 +36,19 @@ def create_post(request: schemas.Post, db: Session = Depends(get_db)):
     return new_post
 
 
-@app.get("/posts/{post_id}", response_model=schemas.Post)
-def read_a_posts(post_id:int, db: Session = Depends(get_db)):
-    posts = db.query(models.Post).filter(models.Post.post_id == post_id).first()
+@app.get("/posts/{post_id}", status_code=status.HTTP_200_OK)
+def read_a_posts(post_id: int, db: Session = Depends(get_db)):
+    posts = db.query(models.Post).filter(models.Post.id == post_id).first()
+    if not posts:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Blog with {post_id} not found",
+        )
     return posts
+
+@app.delete("/post/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(post_id: int, db: Session = Depends(get_db)):
+    db.query(models.Post).filter(models.Post.id == post_id).delete(synchronize_session=False)
+    db.commit()
+    return "Post deleted"
+
