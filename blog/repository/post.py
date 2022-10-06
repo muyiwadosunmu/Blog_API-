1d69d72cf659b2b5fcf9ddf1b6399a72c568d9a9
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-10-06 12:14:02 +0100

    Get all postsand create post refractored

--- blog/repository/post.py
+++ blog/repository/post.py
@@ -4,3 +4,10 @@ from .. import schemas, models
 def get_all_post(db:Session):
     posts = db.query(models.Post).all()
     return posts
+
+def create_a_post(request:schemas.Post, db:Session):
+    new_post = models.Post(title=request.title, body=request.body, user_id=1)
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
--- blog/routers/post.py
+++ blog/routers/post.py
@@ -5,11 +5,9 @@ from .. import schemas, models, database
 from sqlalchemy.orm import Session
 from ..repository import post
 
-router = APIRouter(
-    
-    tags=["Posts"]
-)
+router = APIRouter(tags=["Posts"])
 get_db = database.get_db
+
 
 @router.get(
     "/posts",
@@ -19,21 +17,13 @@ get_db = database.get_db
 def read_posts(db: Session = Depends(database.get_db)):
     return post.get_all_post(db)
 
+
 @router.post("/post", status_code=status.HTTP_201_CREATED)
-def create_post(
-    request: schemas.Post,
-    db: Session = Depends(get_db),
-):
-    new_post = models.Post(title=request.title, body=request.body, user_id=1)
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-    return new_post
+def create_post(request: schemas.Post, db: Session = Depends(get_db)):
+    return post.create_a_post(request, db)
 
 
-@router.delete(
-    "/post/{post_id}", status_code=status.HTTP_204_NO_CONTENT
-)
+@router.delete("/post/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(post_id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == post_id)
     if not post.first():
